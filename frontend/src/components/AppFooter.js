bf16ab634e76f5542feff187353c1a5fc0aa6f40
DaeLee <masterkorea01@naver.com>
2020-06-19 08:53:52 +0900

    Routing

---
+++ frontend/src/components/AppFooter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./AppFooter.scss";
+
+function AppFooter() {
+  return <div className="AppFooter">App Footer</div>;
+}
+
+export default AppFooter;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+import Root from "./pages";
+
+ReactDOM.render(
+  <Router>
+    <Root />
+  </Router>,
+  document.getElementById("root")
+);
