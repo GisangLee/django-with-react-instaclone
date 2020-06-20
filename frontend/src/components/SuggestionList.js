e0e09a8d0f4fc09b462811a5b905511adc5e110a
DaeLee <masterkorea01@naver.com>
2020-06-20 12:13:27 +0900

    useAxios Hook

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { Card } from "antd";
-import "./SuggestionList.scss";
+import React from "react";
+import { Card, notification } from "antd";
+import { FrownOutlined } from "@ant-design/icons";
 import Suggestion from "./Suggestion";
-import axios from "axios";
+import useAxios from "axios-hooks";
 import { useAppContext } from "../store";
+import "./SuggestionList.scss";
 
 const SuggestionList = ({ style }) => {
   const {
     store: { jwtToken },
   } = useAppContext();
 
-  const [userList, setUserList] = useState([]);
+  const headers = { Authorization: `JWT ${jwtToken}` };
 
-  useEffect(() => {
-    async function fetchUserList() {
-      const apiUrl = "http://localhost:8000/accounts/suggestions/";
-      const headers = { Authorization: `JWT ${jwtToken}` };
-      try {
-        const { data } = await axios.get(apiUrl, { headers });
-        setUserList(data);
-        console.log("data:", data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchUserList();
-  }, []);
+  const apiUrl = "http://localhost:8000/accounts/suggestions/";
+
+  const [{ data: userList, loading, error }, refetch] = useAxios({
+    url: apiUrl,
+    headers,
+  });
+
   return (
     <div style={style}>
       <Card title="친구 추천" size="small">
-        {userList.map((suggestionUser) => (
-          <Suggestion
-            key={suggestionUser.username}
-            suggestionUser={suggestionUser}
-          />
-        ))}
+        {loading && <div></div>}
+        {error &&
+          notification.open({
+            message: "로딩 중 에러가 발생했습니다.",
+            description: "에러 발생",
+            icon: <FrownOutlined style={{ color: "#ff3333" }} />,
+          })}
+        <button onClick={() => refetch()}>Reload</button>
+        {userList &&
+          userList.map((suggestionUser) => (
+            <Suggestion
+              key={suggestionUser.username}
+              suggestionUser={suggestionUser}
+            />
+          ))}
       </Card>
     </div>
   );
